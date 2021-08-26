8887d04379d22fa8b76ae6488c3e50e84ac25ede
Aaron Fisher (S3840619) <s3840619@student.rmit.edu.au>
2021-08-26 11:17:48 +1000

    Modified InvalidLoginResponse to indicate false entry more clearly

--- BackEnd/loginmicroservices/src/main/java/com/rmit/sept/bk_loginservices/exceptions/InvalidLoginResponse.java
+++ BackEnd/loginmicroservices/src/main/java/com/rmit/sept/bk_loginservices/exceptions/InvalidLoginResponse.java
@@ -6,8 +6,8 @@ public class InvalidLoginResponse {
         private String password;
 
         public InvalidLoginResponse() {
-            this.username = "Invalid Username";
-            this.password = "Invalid Password";
+            this.username = "Invalid Username (YEP)";
+            this.password = "Invalid Password (NOPE)";
         }
 
         public String getUsername() {
