92dfe40501bdd1c509010f30eca0a45bae0b4033
Oliver Hale <s3781403@student.rmit.edu.au>
2021-09-09 04:17:22 +1000

    add project microservice structure for books

---
+++ BackEnd/booksmicroservices/src/main/java/com/rmit/sept/booksmicroservices/BooksMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.rmit.sept.booksmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksMicroservicesApplication.class, args);
+    }
+
+}
