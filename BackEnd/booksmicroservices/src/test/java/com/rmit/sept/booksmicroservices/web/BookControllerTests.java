de2900458395a9ce7a9a28a49cdda9f74efc7b44
s3781403 <s3781403@student.rmit.edu.au>
2021-09-17 00:51:36 +1000

    add base test packages

---
+++ BackEnd/booksmicroservices/src/test/java/com/rmit/sept/booksmicroservices/web/BookControllerTests.java
@@ -0,0 +1,12 @@
+package com.rmit.sept.booksmicroservices.web;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BookControllerTests {
+
+    @Test
+
+
+}
