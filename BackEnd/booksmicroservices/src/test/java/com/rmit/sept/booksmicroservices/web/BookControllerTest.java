b59a2fad71e321243c85ad22bae31e95e5d01e1f
s3781403 <s3781403@student.rmit.edu.au>
2021-09-25 23:33:41 +1000

    add tests for create read update delete

--- BackEnd/booksmicroservices/src/test/java/com/rmit/sept/booksmicroservices/web/BookControllerTest.java
+++ BackEnd/booksmicroservices/src/test/java/com/rmit/sept/booksmicroservices/web/BookControllerTest.java
@@ -6,29 +6,29 @@ import com.rmit.sept.booksmicroservices.BooksmicroservicesApplicationTests;
 import com.rmit.sept.booksmicroservices.model.MockBook;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+//TODO
+// *The update, delete, create methods need to use mockito instead of directly querying the database
+// and rolling back.
 
 public class BookControllerTest extends BooksmicroservicesApplicationTests {
 
     MockMvc mockMvc;
     @Autowired
     ObjectMapper objectMapper;
-//    @MockBean
-//    BookRepository bookRepository;
-//    @MockBean
-//    BookService bookService;
-//    @MockBean
-//    MapValidationErrorService mapValidationErrorService;
-//    @MockBean
-//    BookValidator bookValidator;
 
     @Autowired
     WebApplicationContext webApplicationContext;
@@ -37,43 +37,111 @@ public class BookControllerTest extends BooksmicroservicesApplicationTests {
     private MockBook book_1;
     private MockBook book_2;
     private MockBook book_3;
+    private MockBook book_4;
 
     @BeforeEach
     public void setup() {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 
-        book_1 = new MockBook(123456789112L, "Test Book #1", "Fantesty",
+        book_1 = new MockBook(1234567891123L, "Test Book #1", "Fantesty",
                 "Rex Testingham", "Penguin Tests", 10.50, "physical", 0,
                 1, 123155, "aws.testimageurl.com/1234-153-1235_source.jpg" );
 
-        book_2 = new MockBook(234567891112L, "Test Book #2", "Horror",
+        book_2 = new MockBook(2345678911123L, "Test Book #2", "Horror",
                 "Sir Tests", "Random House Tests", 20.30, "ebook", 2,
                 4, 234567, "aws.testimageurl.com/1236-143-1135_source.jpg" );
 
-        book_3 = new MockBook(314467823112L, "Test Book #3", "Mystesty",
+        book_3 = new MockBook(3144678231123L, "Test Book #3", "Mystesty",
+                "Mr Tests", "Amazon Tests", 40.50, "physical", 3,
+                1, 5618120, "aws.testimageurl.com/4123-123-151235a.jpg" );
+
+        book_4 = new MockBook(3145327678231123L, "Test Book #3", "Mystesty",
                 "Mr Tests", "Amazon Tests", 40.50, "physical", 3,
                 1, 5618120, "aws.testimageurl.com/4123-123-151235a.jpg" );
     }
 
-
-    //Tests that the get all method works
-//    @Test
-//    @DisplayName("Tests that the getAll method returns OK (successful)")
-//    public void whenGetAllBooks_thenReturnOk() throws Exception {
-//        List<Book> books = new ArrayList<>(Arrays.asList(book_1, book_2, book_3));
-//        Mockito.when(bookRepository.findAll()).thenReturn(books);
-//
-//        //TODO *Unfinished
-//
-//    }
-
+//Create
     @Test
+    @Transactional
     public void whenCreateBook_thenReturnCreated() throws Exception {
         mockMvc.perform(postJson("/api/books/create", book_1))
                 .andExpect(status().isCreated());
     }
 
-    //Helper method for posts
+    @Test
+    @Transactional
+    public void whenCreateBook_thenReturnFailed() throws Exception {
+        mockMvc.perform(postJson("/api/books/create", book_4))
+                .andExpect(status().isBadRequest());
+    }
+
+//Read
+    @Test
+    public void whenGetBook_thenReturnBook() throws Exception {
+//        Mockito.when(bookService.findAll()).thenReturn(books);
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/books/book/3")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void whenGetBook_thenNotReturnBook() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/books/book/-3")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+//Update
+
+    //This should be run using mockito and not actually query the database
+    @Test
+    @Transactional
+    public void whenUpdateBook_thenBookIsUpdated() throws Exception {
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.put("/api/books/update/35")
+                .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(book_2));
+
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isOk());
+    }
+
+
+    //Should be updated to use mockito instead
+    @Test
+    @Transactional
+    public void whenUpdateBook_thenBookIsNotUpdated() throws Exception {
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.put("/api/books/update/34")
+                .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(book_4));
+
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isBadRequest());
+    }
+
+//Delete
+    @Test
+    @Transactional
+    public void whenDeleteBook_bookIsDeleted() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .delete("/api/books/delete/33")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @Transactional
+    public void whenDeleteBook_noBookIsDeleted() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .delete("/api/books/delete/0")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    //Helper method for post requests
     private static MockHttpServletRequestBuilder postJson(String uri, Object body) {
         try {
             String json = new ObjectMapper().writeValueAsString(body);
