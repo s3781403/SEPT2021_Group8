b9d75aa69627f367d8bcb6d1a1c35129f91300d6
shrestha ghosh <s3805530@student.rmit.edu.au>
2021-10-24 15:47:57 +1100

    Update shopping cart

--- FrontEnd/myfirstapp/src/ui/pages/ShoppingCart.js
+++ FrontEnd/myfirstapp/src/ui/pages/ShoppingCart.js
@@ -6,25 +6,21 @@ import BookCardMaterial from "../components/BookCardMaterial";
 import PayPal from "./PayPal";
 
 function CartItemSummary({item}) {
-    // Book card
-    // Qty
-
 
 
     return (
     <div>
         <Grid container>
-            <Grid item xs={12} md={8}>
+            <Grid item xs={12} md={4} >
                  <BookCardMaterial book={item} />
             </Grid>
-            <Grid item xs={12} md={4} style={{textAlign: 'center'}}>
+            <Grid item xs={12} md={8} style={{textAlign: 'center'}}>
                 <label>Quantity</label><br/>
                 <input type={"number"} value={item.qty}/>
             </Grid>
 
         </Grid>
         <hr/>
-
     </div>
 
     )
