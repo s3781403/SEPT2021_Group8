beb55b7e23f7645e7c2ab66d3d62ccee86bafcd7
Aaron Fisher (S3840619) <s3840619@student.rmit.edu.au>
2021-10-25 17:18:54 +1100

    Added individual docker image paths to circleCI config

--- BackEnd/ordersmicroservice/src/main/java/com/rmit/sept/ordersmicroservice/model/LineItem.java
+++ BackEnd/ordersmicroservice/src/main/java/com/rmit/sept/ordersmicroservice/model/LineItem.java
@@ -23,20 +23,6 @@ public class LineItem {
 
     private long bookID;
 
-    private long isbn;
-    private String title;
-    private String category;
-    private String author;
-    private String publisher;
-    private String description;
-    private double price;
-    private String type;
-    private int quality;
-    private int stock;
-    private long sellerID;
-    private String imageURL;
-    private String pdfURL;
-
     public Date getCreate_At() {return create_At;}
 
     public void setCreate_At(Date create_At) {this.create_At = create_At;}
@@ -79,111 +65,6 @@ public class LineItem {
     @PreUpdate
     protected void onUpdate(){
         this.update_At = new Date();
-    }
-
-
-    public long getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(long isbn) {
-        this.isbn = isbn;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public int getQuality() {
-        return quality;
-    }
-
-    public void setQuality(int quality) {
-        this.quality = quality;
-    }
-
-    public int getStock() {
-        return stock;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
-
-    public long getSellerID() {
-        return sellerID;
-    }
-
-    public void setSellerID(long sellerID) {
-        this.sellerID = sellerID;
-    }
-
-    public String getImageURL() {
-        return imageURL;
-    }
-
-    public void setImageURL(String imageURL) {
-        this.imageURL = imageURL;
-    }
-
-    public String getPdfURL() {
-        return pdfURL;
-    }
-
-    public void setPdfURL(String pdfURL) {
-        this.pdfURL = pdfURL;
     }
 
 }
--- BackEnd/ordersmicroservice/src/main/java/com/rmit/sept/ordersmicroservice/service/LineItemService.java
+++ BackEnd/ordersmicroservice/src/main/java/com/rmit/sept/ordersmicroservice/service/LineItemService.java
@@ -1,6 +1,5 @@
 package com.rmit.sept.ordersmicroservice.service;
 
-import com.rmit.sept.ordersmicroservice.model.BookDTO;
 import com.rmit.sept.ordersmicroservice.model.Cart;
 import com.rmit.sept.ordersmicroservice.model.LineItem;
 import com.rmit.sept.ordersmicroservice.repositories.LineItemRepository;
@@ -25,23 +24,6 @@ public class LineItemService {
             newLineItem.setCart(cart);
             cart.setUpdate_At(new Date());
             newLineItem.setBookID(newLineItem.getBookID());
-            String getBookURL = "http://localhost:8081/api/books/book/"+newLineItem.getBookID();
-            BookDTO bookInfo = restTemplateBuilder.build().getForObject(getBookURL, BookDTO.class);
-            newLineItem.setIsbn(bookInfo.getIsbn());
-            newLineItem.setAuthor(bookInfo.getAuthor());
-            newLineItem.setCategory(bookInfo.getCategory());
-            newLineItem.setPrice(bookInfo.getPrice());
-            newLineItem.setPublisher(bookInfo.getPublisher());
-            newLineItem.setDescription(bookInfo.getDescription());
-            newLineItem.setSellerID(bookInfo.getSellerID());
-            newLineItem.setTitle(bookInfo.getTitle());
-            newLineItem.setType(bookInfo.getType());
-            newLineItem.setCreate_At(bookInfo.getCreate_At());
-            newLineItem.setId(bookInfo.getId());
-            newLineItem.setQuality(bookInfo.getQuality());
-            newLineItem.setStock(bookInfo.getStock());
-            newLineItem.setImageURL(bookInfo.getImageURL());
-            newLineItem.setPdfURL(bookInfo.getPdfURL());
             newLineItem.setQuantity(newLineItem.getQuantity());
 
             return lineItemRepository.save(newLineItem);
